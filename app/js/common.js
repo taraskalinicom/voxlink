54da4b56e60ae9f577ba11778d7da63cbc513d74
taraskalinicom <taraskalinicom@gmail.com>
2017-10-14 19:24:19 +0300

    taraskalinicom

--- app/js/common.js
+++ app/js/common.js
@@ -51,6 +51,42 @@ $(function() {
 
     $("a[rel='m_PageScroll2id']").mPageScroll2id();
 
+    var ipSlider = $('#ip').lightSlider({
+        item:3,
+        loop:false,
+        controls:false,
+        slideMove:2,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed:600,
+        responsive : [
+            {
+                breakpoint:800,
+                settings: {
+                    item:3,
+                    slideMove:1,
+                    slideMargin:6,
+                }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:2,
+                    slideMove:1
+                }
+            }
+        ]
+    });
+
+    $(".ip-prev").on("click",function(e){
+        e.preventDefault();
+        ipSlider.goToPrevSlide();
+    });
+
+    $(".ip-next").on("click",function(e){
+        e.preventDefault();
+        ipSlider.goToNextSlide();
+    });
+
 });
 
 //Форма отправки 2.0
